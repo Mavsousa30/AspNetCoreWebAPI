fdaa2e8f37cce3ae3cc8fe883c1a9cd16912ac7f
marco.sousa <mavsousa@gmail.com>
2021-07-26 20:49:01 -0300

    Adding my practice project on GitHub

---
+++ SmartSchool.API/Controllers/AlunoController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartSchool.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SmartSchool.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+        public List<Aluno> Alunos = new List<Aluno>()
+        {
+            new Aluno()
+            {
+                Id = 1,
+                Nome = "Marco",
+                Sobrenome = "Sousa",
+                Telefone = "32988881111"
+            },
+            new Aluno()
+            {
+                Id = 2,
+                Nome = "Enzo",
+                Sobrenome = "Vilela",
+                Telefone = "32988882222"
+            },
+            new Aluno()
+            {
+                Id = 3,
+                Nome = "Thaís",
+                Sobrenome = "Cravo",
+                Telefone = "32988883333"
+            },
+        };
+
+
+        // GET: api/<AlunoController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Alunos);
+        }
+
+        // GET api/<AlunoController>/5
+        [HttpGet("byId/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var aluno = Alunos.FirstOrDefault(a => a.Id == id);
+            if (aluno == null) return BadRequest("Aluno não encontrado");
+            return Ok(aluno);
+        }
+
+        // POST api/<AlunoController>
+        [HttpPost]
+        public IActionResult Post(Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        // PUT api/<AlunoController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        // DELETE api/<AlunoController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
